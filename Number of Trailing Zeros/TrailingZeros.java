935f2e3a8c64713d1dfc8d3db54e7dd52d4a85f2
thinkman1 <dcai1987@gmail.com>
2014-04-20 22:43:16 -0400

    commit

---
+++ Number of Trailing Zeros/TrailingZeros.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author thinkman
+ *
+ */
+public class TrailingZeros {
+	public int numOfTrailingZeros(int n) {
+		if (n < 0) return -1;
+		int count = 0;
+		for (int i = 5; n / i > 0; i *= 5) {
+			count += n / i;
+		}
+		return count;
+	}
+}
